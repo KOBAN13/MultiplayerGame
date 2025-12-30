76e2d1f7c824f770be343a78489c57c4864e1574
Daniil <layk.danil@mail.ru>
2025-12-30 22:44:58 +0400

    WIP Projectile

--- Assets/Scripts/Db/Pool/PoolData.cs
+++ Assets/Scripts/Db/Pool/PoolData.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using Db.Dictionary;
 using Db.Interface;
 using Sirenix.OdinInspector;
 using Sirenix.Serialization;
@@ -11,8 +10,8 @@ namespace Db.Pool
     [CreateAssetMenu(fileName = "PoolData", menuName = "Db/PoolData")]
     public class PoolData : SerializedScriptableObject, IPoolData
     {
-        [OdinSerialize] private ObjectPoolsDictionary objectPoolsDictionary;
+        [OdinSerialize] private Dictionary<EObjectInPoolName, PoolEntry> _objectPoolsDictionary;
         
-        public IReadOnlyDictionary<EObjectInPoolName, PoolEntry> ObjectPoolsDictionary => objectPoolsDictionary;
+        public IReadOnlyDictionary<EObjectInPoolName, PoolEntry> ObjectPoolsDictionary => _objectPoolsDictionary;
     }
 }
