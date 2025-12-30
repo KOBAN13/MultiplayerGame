8ddf8e630dddf5f8d1aa28f57a22178026b0fa92
Daniil <layk.danil@mail.ru>
2025-12-30 22:22:27 +0400

    Import odin

--- Assets/Scripts/Db/Pool/PoolData.cs
+++ Assets/Scripts/Db/Pool/PoolData.cs
@@ -1,16 +1,17 @@
 ﻿using System.Collections.Generic;
 using Db.Dictionary;
 using Db.Interface;
+using Sirenix.OdinInspector;
+using Sirenix.Serialization;
 using UnityEngine;
-using UnityEngine.Serialization;
 using Utils.Enums;
 
 namespace Db.Pool
 {
     [CreateAssetMenu(fileName = "PoolData", menuName = "Db/PoolData")]
-    public class PoolData : ScriptableObject, IPoolData
+    public class PoolData : SerializedScriptableObject, IPoolData
     {
-        [SerializeField] private ObjectPoolsDictionary objectPoolsDictionary;
+        [OdinSerialize] private ObjectPoolsDictionary objectPoolsDictionary;
         
         public IReadOnlyDictionary<EObjectInPoolName, PoolEntry> ObjectPoolsDictionary => objectPoolsDictionary;
     }
